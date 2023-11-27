aeb321916c68faac20e50899e73c8f170231a788
Jonte88 <jonathan.thj15@gmail.com>
2023-11-27 10:34:34 +0100

    Removing wrong stuff 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import { useState } from "react";
-import placeholder_person from "./assets/images/placeholder_person.png";
 import "./App.css";
+import Header from "./header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <header>
-        <div className="leftNavDiv">
-          <div className="leftNavDivUp">NTI Gymnasiet</div>
-          <div className="leftNavDivDown">Personal</div>
-        </div>
-        <div className="rightNavDiv">
-          <img src={placeholder_person} alt="Person photo" />
-        </div>
-      </header>
+      <Header />
       <div></div>
     </>
   );
