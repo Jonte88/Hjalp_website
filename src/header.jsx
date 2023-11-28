a2aab03c53ff47b923020c4cc857df02b22cb2cb
Jonte88 <jonathan.thj15@gmail.com>
2023-11-28 08:50:36 +0100

    Adding header and setting up website 	modified:   index.html 	modified:   src/App.jsx 	new file:   src/assets/css/header.css 	new file:   src/assets/images/logo.png 	modified:   src/header.jsx

--- src/header.jsx
+++ src/header.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+// import { useState } from "react";
 import placeholder_person from "./assets/images/placeholder_person.png";
-import "./App.css";
+import "./assets/css/header.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+//   const [count, setCount] = useState(0);
 
   return (
     <>
@@ -14,6 +14,7 @@ function App() {
         </div>
         <div className="rightNavDiv">
           <img src={placeholder_person} alt="Person photo" />
+          <h4>Admin Admin</h4>
         </div>
       </header>
     </>
